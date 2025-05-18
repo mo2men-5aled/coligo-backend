a59a3a4bd9b1e7d9f0b9c490188f3bf65fb2fa2f
Momen Khaled <momenk208@gmail.com>
2025-05-18 21:25:21 +0300

    added the live url to the allowed origins

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   // Enable CORS with proper configuration
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173',"https://coligo-frontend-alce-in4k7mzl4-mo2men5aleds-projects.vercel.app"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // If you need to allow cookies/auth headers
     allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
